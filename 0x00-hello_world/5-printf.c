48f885b34d36736faf8b754972c994aaec097a27
Edleychris <edleychris.ec@gmail.com>
2023-02-21 00:10:52 -0800

    proc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - A programm that prints a line using the prinft function
-* Return: 0 (Success)
+ * main - A programm that prints a line using the prinft function
+ *  Return: 0 (Success)
 */
 int main(void)
 {
